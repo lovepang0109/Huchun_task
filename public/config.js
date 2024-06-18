0e73a6254bea494064b59cfffa7876c76d336703
lovepang0109@gmail.com <lovepang0109@gmail.com>
2024-06-18 06:21:08 -0700

    yyy

--- public/config.js
+++ public/config.js
@@ -2,11 +2,11 @@ var configData = {
     /* 开发配置 */
     // VUE_APP_BASE_API: 'https://panel.nvia.com/api/v1',
     // APP_BASE_URL:     "https://panel.nvia.com/",
-    // VUE_APP_BASE_API: 'http://localhost:8001/api/v1',
-    // APP_BASE_URL:     "http://localhost:8001/",
+    VUE_APP_BASE_API: 'http://localhost:8001/api/v1',
+    APP_BASE_URL:     "http://localhost:8001/",
 
-    VUE_APP_BASE_API: 'https://huchun-task.vercel.app/api/v1',
-    APP_BASE_URL:     "https://huchun-task.vercel.app/",
+    // VUE_APP_BASE_API: 'https://huchun-task.vercel.app/api/v1',
+    // APP_BASE_URL:     "https://huchun-task.vercel.app/",
     
 
     SystemName: 'PanelSMS',
