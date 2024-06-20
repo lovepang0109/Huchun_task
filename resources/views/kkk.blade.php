d674908e698d14b167c1370f9366b026949181b3
lovepang0109@gmail.com <lovepang0109@gmail.com>
2024-06-19 19:57:30 -0700

    yyy

--- resources/views/kkk.blade.php
+++ resources/views/kkk.blade.php
@@ -7,6 +7,13 @@
 	echo "This is laravel first app project";
 
 	echo $path;
+
+	use Illuminate\Support\Facades\File;
+
+    $path = public_path();
+    $files = File::allFiles($path);
+  
+    dd($files);
 ?>
 
 </h1>
