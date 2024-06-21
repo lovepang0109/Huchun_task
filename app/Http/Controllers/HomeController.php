e46067f3811436920b920377112b1e52cef11a5f
lovepang0109@gmail.com <lovepang0109@gmail.com>
2024-06-21 00:14:10 -0700

    cdcdc

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
     //   );
 
      
-        return view('kkk', ['path'=>$result['url']]);
+        return view('kkk', ['path'=>$result->url]);
         // return view('kkk', ['path'=>$result]);
 
     }
