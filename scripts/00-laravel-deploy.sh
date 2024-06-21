42b04a071c31bfc3b81846317187080412e1be4b
lovepang0109@gmail.com <lovepang0109@gmail.com>
2024-06-20 19:15:30 -0700

    dede

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html
+
+
+echo "Running start server..."
+php artisan serve 
+
+#echo "Running migrations..."
+#php artisan migrate --force
+
+#echo "Running seeders..."
+#php artisan db:seed
+
+#echo "Running vite..."
+#npm install
+#npm run build
